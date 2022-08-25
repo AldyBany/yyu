edef76c483800cbabd59e259f95c5e992ae505cb
Gaick Bany <banygaick@gmail.com>
2022-08-25 11:42:04 +0200

    index file updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,13 @@
-
-import Banner from 'sections/Banner'
-
-import Services from 'sections/services'
-import Navbar from 'components/n'
-import Swipers from 'components/swiper2'
-import Lide from 'components/swiper'
-import Frame from 'components/frame'
-import Footer from 'sections/footer'
-import Emergency from 'sections/emergency'
-import Proof from 'sections/proof'
-import About from 'sections/about'
-import T from 'data/t'
-import Item from 'components/Item'
-
-import React, {useState} from 'react'
-
-import SmoothScroll from 'components/SmoothScroll'
-
-
+import Footer from '../sections/footer'
 
 export default function Home() {
-
-  const[cliked,setCliked] =useState(false)
   return (
-    <>
-    
-    <div className='border-box m-0 p-0'>
-    
-      <Navbar/>
-      <Banner/>
-      {/* <Lide/> */}
-      <About/>
-      {/* <Item/> */}
-      {/* <Swipers/> */}
-      {/* <Fade/> */}
-     
-      
-      
-      <Services/>
-      <Emergency/>
-      <Proof/>
+    <div>
       <Footer/>
-    </div>
-    {/* <SmoothScroll/> */}
-    </>
-  )
-}
 
-const styles={
- 
+     
+
+      
+    </div>
+  )
 }
