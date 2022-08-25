ffcddefd911dbcecb685fa83e90f2716d1c2d15d
Gaick Bany <banygaick@gmail.com>
2022-08-25 13:15:38 +0200

    index updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,44 @@
-import Footer from '../sections/footer'
+
+import Banner from 'sections/Banner'
+
+import Services from 'sections/services'
+import Navbar from 'components/n'
+
+
+import Footer from 'sections/footer'
+import Emergency from 'sections/emergency'
+import Proof from 'sections/proof'
+import About from 'sections/about'
+
+
+import React, {useState} from 'react'
+
+
+
 
 export default function Home() {
+
+  const[cliked,setCliked] =useState(false)
   return (
-    <div>
-      <Footer/>
-
+    <>
+    
+    <div className='border-box m-0 p-0'>
+    
+      <Navbar/>
+      <Banner/>
+      <About/>
      
-
+     
       
+      
+      <Services/>
+      <Emergency/>
+      <Proof/>
+      <Footer/>
     </div>
+    
+    </>
   )
 }
+
+
